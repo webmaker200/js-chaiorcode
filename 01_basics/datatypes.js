ff82b2b7bdd76f24ee5005bcc7c1d9ec0b79eca4
Usman Jamil <113774697+webmaker200@users.noreply.github.com>
2024-02-01 18:52:12 +0000

    Datatypes Completed

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,5 @@
+"use strict";   //treat all js code as newer version
+
+// alert("hello") // we cannot use this in node js enviornment
+let name = "Usman" //string datatype
+let age   = 20 // int datatype
